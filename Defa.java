d32b8d7f932742ce3c8047f89ea316ce9ea0cced
Frkn2344 <98692779+Frkn2344@users.noreply.github.com>
2024-05-09 18:08:07 +0300

    Add files via upload

---
+++ Defa.java
@@ -0,0 +1,13 @@
+
+package pkg5.defa;
+import java.util.Scanner;
+public class Defa {
+ public static void main(String[] args) {
+   int   i=5;
+      while(i>0)
+      {
+        System.out.println("hello java");
+         i=i-1;   
+      }
+    }   
+}
---
+++ Diziornek4.java
@@ -0,0 +1,14 @@
+
+package diziornek44;
+import java.util.Scanner;
+public class Diziornek44;
+    public static void main(String[] args) {
+     Scanner klavye= new Scanner(System.in); 
+        System.out.println("birinci cümleyi giriniz");
+      String s1=klavye.nextLine ();
+        System.out.println("ikinci cümleyi giriniz");  
+     String  s2=klavye.nextLine();
+        System.out.println(s1.concat(s2));
+    }
+    
+}
